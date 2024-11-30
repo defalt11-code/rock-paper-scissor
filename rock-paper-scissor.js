3692ec655ab6bad250cb9d5efc289d4403c2b152
defalt11-code <royilao1114@gmail.com>
2024-11-30 15:58:16 +0800

    Add computer pick function and that return value of computer move

---
+++ rock-paper-scissor.js
@@ -0,0 +1,18 @@
+function computerPick() {
+  let randomNumber = Math.random();
+  let computerMove = '';
+
+  if(randomNumber > 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  }else if(randomNumber > 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  }else if(randomNumber > 2 / 3 && randomNumber < 1) {
+    computerMove = 'scissor';
+  }
+
+  return computerMove;
+}
+
+console.log(computerPick());
+
+
