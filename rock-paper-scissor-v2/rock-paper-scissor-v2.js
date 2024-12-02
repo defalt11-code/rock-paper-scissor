e372ba5227ca7baa61e09e8620eab12f405715fb
defalt11-code <royilao1114@gmail.com>
2024-12-02 19:18:02 +0800

    Initialize the same function but shorter that version 1 rock paper scissor and add new move array

---
+++ rock-paper-scissor-v2/rock-paper-scissor-v2.js
@@ -0,0 +1,25 @@
+const moves = ['rock', 'paper', 'scissor'];
+
+//Get a random move 
+const getRandomMove = (moves) => moves[Math.floor(Math.random() * moves.length)];
+
+//Map for possible outcome
+let  possibleOutcome= {
+  rock: {rock: 'tie', paper: 'you lose', scissor: 'you win'},
+  paper: {rock: 'you win', paper: 'tie', scissor: 'you lose'},
+  scissor: {rock: 'you lose', paper: 'you lose', scissor: 'tie'}
+};
+
+//This function determine the winner of the game
+function playGame(playerMove) {
+  const computerMove = getRandomMove(moves);
+  
+  let result = possibleOutcome[playerMove]?.[computerMove] || 'Invalid Move';
+
+  console.log(`You ${playerMove} - ${computerMove} Computer pick\n${result}`);
+};
+
+
+
+
+
